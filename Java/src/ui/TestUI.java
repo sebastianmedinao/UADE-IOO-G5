034202b5f886a7f334ecb8645bf154f97b0a7fb0
sebastianmedinao <sebastianmedinao@hotmail.com>
2021-10-18 22:11:59 -0300

    Test UI

---
+++ Java/src/ui/TestUI.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javax.swing.*;
+
+public class TestUI {
+    private JPanel InicioTest;
+    private JLabel lblTest;
+    private JButton button1;
+
+    public static void main(String[] args) {
+        JFrame root = new JFrame("Test Title");
+        root.setContentPane(new TestUI().InicioTest);
+        root.setLocationRelativeTo(null);
+        root.setSize(400, 400);
+        root.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        root.setVisible(true);
+    }
+}
