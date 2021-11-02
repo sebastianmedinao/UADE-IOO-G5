5397d2c142a2425da8e617de6c69b3687dacb4dc
sebastianmedinao <sebastianmedinao@hotmail.com>
2021-11-02 19:35:54 -0300

    daos

---
+++ Java/src/com/UADE/util/CriterioDAO.java
@@ -0,0 +1,4 @@
+package com.UADE.util;
+
+public class CriterioDAO {
+}
---
+++ Java/src/com/UADE/util/ValorDAO.java
@@ -0,0 +1,10 @@
+package com.UADE.util;
+
+import com.UADE.model.Valor;
+
+public class ValorDAO extends GenericDAO<Valor> {
+
+    public ValorDAO(Class<Valor> clase, String file) throws Exception {
+        super(clase, file);
+    }
+}
