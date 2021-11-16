a360c448b87467977a577d79381f3261411e535f
FariasN <nehuens06@gmail.com>
2021-11-16 19:42:41 -0300

    menu resultados

---
+++ Java/src/com/UADE/view/ResultadosUI.java
@@ -0,0 +1,9 @@
+package com.UADE.view;
+
+import javax.swing.*;
+
+public class ResultadosUI {
+    private JButton crearResultadoButton;
+    private JButton modificarConsultarResultadoButton;
+    private JButton eliminarResultadoButton;
+}
