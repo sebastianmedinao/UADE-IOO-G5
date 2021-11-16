3f34646ed12fe0ffc5046340498db8fa1541c6f1
Carla <88563106+carlablanco@users.noreply.github.com>
2021-11-16 17:23:40 -0300

    nuevo paciente UI

--- Java/src/com/UADE/controller/ResultadoPracticaController.java
+++ Java/src/com/UADE/controller/ResultadoPracticaController.java
@@ -1,4 +1,21 @@
 package com.UADE.controller;
 
+import com.UADE.dao.PracticaDAO;
+import com.UADE.dao.ResultadoPracticaDAO;
+import com.UADE.model.Practica;
+import com.UADE.model.ResultadoPractica;
+
+import java.util.List;
+
 public class ResultadoPracticaController {
+    private List<ResultadoPractica> resultadoPracticas;
+    private final ResultadoPracticaDAO DAO;
+
+    public PracticaController() throws Exception {
+        DAO = new ResultadoPracticaDAO()DAO(ResultadoPractica.class, "dao/ResultadoPractica.dao");
+        resultadoPracticas = DAO.getAll();
 }
+    private Integer getNuevoCodigo() {
+        ResultadoPractica lastpract = practicas.get(practicas.size() - 1);
+        return lastpract.getCodigo() + 1;
+    }
