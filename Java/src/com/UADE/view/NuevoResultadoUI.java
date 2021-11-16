5ca89a43ec66a104ea5710034d8d11a09b5f7cca
FariasN <nehuens06@gmail.com>
2021-11-16 19:36:16 -0300

    nuevo resultado

--- Java/src/com/UADE/view/NuevoResultadoUI.java
+++ Java/src/com/UADE/view/NuevoResultadoUI.java
@@ -1,4 +1,11 @@
 package com.UADE.view;
 
+import javax.swing.*;
+
 public class NuevoResultadoUI {
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField textField2;
+    private JTextField textField1;
+    private JTextField textField3;
 }
