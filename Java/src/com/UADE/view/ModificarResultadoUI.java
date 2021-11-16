3ed221f710dcf1b524d62666900f60fa0b2a7e72
FariasN <nehuens06@gmail.com>
2021-11-16 20:10:53 -0300

    menu resultados

---
+++ Java/src/com/UADE/view/ModificarResultadoUI.java
@@ -0,0 +1,10 @@
+package com.UADE.view;
+
+import javax.swing.*;
+
+public class ModificarResultadoUI {
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton modificarConsultarButton;
+    private JButton eliminarButton;
+}
--- Java/src/com/UADE/view/NuevoResultadoUI.java
+++ Java/src/com/UADE/view/NuevoResultadoUI.java
@@ -6,6 +6,8 @@ public class NuevoResultadoUI {
     private JComboBox comboBox1;
     private JComboBox comboBox2;
     private JTextField textField2;
-    private JTextField textField1;
     private JTextField textField3;
+    private JTextArea textArea1;
+    private JButton aceptarButton;
+    private JButton cancelarButton;
 }
