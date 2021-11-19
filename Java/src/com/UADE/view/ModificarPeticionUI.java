a927c577bef339e0b0670bdef60ad96bb207c3c4
Carla <88563106+carlablanco@users.noreply.github.com>
2021-11-19 15:13:04 -0300

    aa

--- Java/src/com/UADE/view/ModificarPeticionUI.java
+++ Java/src/com/UADE/view/ModificarPeticionUI.java
@@ -60,17 +60,6 @@ public class ModificarPeticionUI {
             }
         });
 
-        resultadosButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    //new ResultadosUI();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
 
     }
 }
